2ee35b37e94c86c24dadc8c4defa3320c649dc40
jlamimoso <jlamimoso@yahoo.com.br>
2015-01-14 14:17:32 -0200

    Teste 1
    Desc 1

--- tail.go
+++ tail.go
@@ -3,12 +3,44 @@ package main
 import (
 	"fmt"
 	"github.com/ActiveState/tail"
+	"github.com/go-fsnotify/fsnotify"
+	"log"
 	"os"
 )
 
+func ExampleNewWatcher(filename string) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
+
 func main() {
 	fmt.Println("iniciando tail")
-	t, err := tail.TailFile("teste.txt", tail.Config{Follow: true, ReOpen: true})
+	ExampleNewWatcher("C:\\Temp\\wq-64Bits\\log\\WQ_AUDIT_XXPC_P9001_14012015.csv")
+	t, err := tail.TailFile("C:\\Temp\\wq-64Bits\\log\\WQ_AUDIT_XXPC_P9001_14012015.csv", tail.Config{Follow: true, ReOpen: true})
 	if err != nil {
 		fmt.Printf("Erro ... %s", err)
 		os.Exit(1)
