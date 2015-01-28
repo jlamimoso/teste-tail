3bf6dead9016a352bd093e91dcfa13b655c403a4
jlamimoso <jlamimoso@Jose-Luiz-Alla-Mimosos-MacBook-Pro.local>
2015-01-27 23:08:20 -0200

    Teste
    Desc 1

--- tail.go
+++ tail.go
@@ -41,6 +41,7 @@ func main() {
 	fmt.Println("iniciando tail")
 	ExampleNewWatcher("C:\\Temp\\wq-64Bits\\log\\WQ_AUDIT_XXPC_P9001_14012015.csv")
 	t, err := tail.TailFile("C:\\Temp\\wq-64Bits\\log\\WQ_AUDIT_XXPC_P9001_14012015.csv", tail.Config{Follow: true, ReOpen: true})
+
 	if err != nil {
 		fmt.Printf("Erro ... %s", err)
 		os.Exit(1)
@@ -48,4 +49,5 @@ func main() {
 	for line := range t.Lines {
 		fmt.Printf("linha -> %s\n", line.Text)
 	}
+
 }
