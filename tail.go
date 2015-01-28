d84e73653787d41de88b3dcc0ffca1d8889543a4
jlamimoso <jlamimoso@yahoo.com.br>
2015-01-28 18:18:15 -0200

    Alt 1
    Desc 1

--- tail.go
+++ tail.go
@@ -3,39 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/ActiveState/tail"
-	"github.com/go-fsnotify/fsnotify"
-	"log"
 	"os"
 )
-
-func ExampleNewWatcher(filename string) {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer watcher.Close()
-
-	done := make(chan bool)
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
-				log.Println("event:", event)
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("modified file:", event.Name)
-				}
-			case err := <-watcher.Errors:
-				log.Println("error:", err)
-			}
-		}
-	}()
-
-	err = watcher.Add(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	<-done
-}
 
 func main() {
 	fmt.Println("iniciando tail")
